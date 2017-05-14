b3d40858a1d80a697a1b8c2bf3d7b6ff6f550f69
steven <stevenle2011@gmail.com>
2017-05-14 10:46:49 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
+app.use(morgan('dev'));
+var port = process.env.PORT || 3000;
+
+var multer = require('multer');
+var upload = multer({dest:'uploads/'})
+
+app.use('/',express.static(__dirname + '/public'));
+
+app.post('/filesize',upload.single('file'),function(req,res){
+	console.log(req.file)
+	res.json({'bytes':req.file.size})
+	
+})
+
+app.listen(port,function(){
+	console.log(`Listening on port ${port}`)
+})
+
